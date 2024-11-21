2b9ba0ad5505ceb103d8684ef0de6b31297e052b
Sebe Norbert Dávid <sebedavid04@gmail.com>
2024-11-21 09:32:58 +0100

    added file handling

--- src/main/java/nagyhazi/FileHandler.java
+++ src/main/java/nagyhazi/FileHandler.java
@@ -1,7 +1,39 @@
 package nagyhazi;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.swing.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 public class FileHandler {
     // { "encryptionType": "Caesar", "input": "hello", "output": "khoor" }
 
     // { "partnerName": "John Doe", "encryptionType": "Caesar" }
+
+    public void save(String input, String key, String output, String encryptionType) throws IOException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("encryptionType", encryptionType);
+        jsonObject.put("input", input);
+        jsonObject.put("output", output);
+        jsonObject.put("key", key);
+        FileWriter fileWriter = new FileWriter("./src/main/resources/output.json");
+        fileWriter.write(jsonObject.toJSONString());
+        fileWriter.close();
+    }
+
+    public void load(JTextField inputField, JTextField keyField, JTextField outputField, JComboBox<String> encryptionType) throws IOException, ParseException {
+        JSONObject jsonObject;
+        FileReader fileReader = new FileReader("./src/main/resources/output.json");
+        jsonObject = (JSONObject) new JSONParser().parse(fileReader);
+        inputField.setText((String) jsonObject.get("input"));
+        keyField.setText((String) jsonObject.get("key"));
+        outputField.setText((String) jsonObject.get("output"));
+        encryptionType.setSelectedItem(jsonObject.get("encryptionType"));
+    }
+
 }
--- src/main/java/nagyhazi/Interface.java
+++ src/main/java/nagyhazi/Interface.java
@@ -16,15 +16,21 @@ public class Interface {
 
         JButton encryptButton = new JButton("Encrypt");
         JButton decryptButton = new JButton("Decrypt");
+        JButton saveButton = new JButton("Save");
+        JButton loadButton = new JButton("Load");
 
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         encryptButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         decryptButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        saveButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loadButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         panel.add(encryptButton);
         panel.add(decryptButton);
+        panel.add(saveButton);
+        panel.add(loadButton);
 
         JTextField inputField = new JTextField();
         inputField.setColumns(10);
@@ -49,72 +55,53 @@ public class Interface {
         encryptButton.addActionListener(e -> {
             Encrypt encrypt = new Encrypt();
             switch (comboBox.getSelectedItem().toString()) {
-                case "Hex":
-                    outputField.setText(encrypt.hex(inputField.getText()));
-                    break;
-                case "Binary":
-                    outputField.setText(encrypt.binary(inputField.getText()));
-                    break;
-                case "Base64":
-                    outputField.setText(encrypt.base64(inputField.getText()));
-                    break;
-                case "Rot13":
-                    outputField.setText(encrypt.rot13(inputField.getText()));
-                    break;
-                case "Caesar":
-                    outputField.setText(encrypt.caesar(inputField.getText(), Integer.parseInt(keyField.getText())));
-                    break;
-                case "Vigenere":
-                    outputField.setText(encrypt.vigenere(inputField.getText(), keyField.getText()));
-                    break;
-                case "Morse":
-                    outputField.setText(encrypt.morse(inputField.getText()));
-                    break;
-                case "Atbash":
-                    outputField.setText(encrypt.atbash(inputField.getText()));
-                    break;
-                case "Bacon":
-                    outputField.setText(encrypt.bacon(inputField.getText()));
-                    break;
-                case "XOR":
-                    outputField.setText(encrypt.xor(inputField.getText() , keyField.getText()));
-                    break;
+                case "Hex" -> outputField.setText(encrypt.hex(inputField.getText()));
+                case "Binary" -> outputField.setText(encrypt.binary(inputField.getText()));
+                case "Base64" -> outputField.setText(encrypt.base64(inputField.getText()));
+                case "Rot13" -> outputField.setText(encrypt.rot13(inputField.getText()));
+                case "Caesar" ->
+                        outputField.setText(encrypt.caesar(inputField.getText(), Integer.parseInt(keyField.getText())));
+                case "Vigenere" -> outputField.setText(encrypt.vigenere(inputField.getText(), keyField.getText()));
+                case "Morse" -> outputField.setText(encrypt.morse(inputField.getText()));
+                case "Atbash" -> outputField.setText(encrypt.atbash(inputField.getText()));
+                case "Bacon" -> outputField.setText(encrypt.bacon(inputField.getText()));
+                case "XOR" -> outputField.setText(encrypt.xor(inputField.getText(), keyField.getText()));
             }
         });
 
         decryptButton.addActionListener(e -> {
             Decrypt decrypt = new Decrypt();
             switch (comboBox.getSelectedItem().toString()) {
-                case "Hex":
-                    outputField.setText(decrypt.hex(inputField.getText()));
-                    break;
-                case "Binary":
-                    outputField.setText(decrypt.binary(inputField.getText()));
-                    break;
-                case "Base64":
-                    outputField.setText(decrypt.base64(inputField.getText()));
-                    break;
-                case "Rot13":
-                    outputField.setText(decrypt.rot13(inputField.getText()));
-                    break;
-                case "Caesar":
-                    outputField.setText(decrypt.caesar(inputField.getText(), Integer.parseInt(keyField.getText())));
-                    break;
-                case "Vigenere":
-                    outputField.setText(decrypt.vigenere(inputField.getText(), keyField.getText()));
-                    break;
-                case "Morse":
-                    outputField.setText(decrypt.morse(inputField.getText()));
-                    break;
-                case "Atbash":
-                    outputField.setText(decrypt.atbash(inputField.getText()));
-                    break;
-                case "Bacon":
-                    outputField.setText(decrypt.bacon(inputField.getText()));
-                    break;
-                case "XOR":
-                    outputField.setText(decrypt.xor(inputField.getText(), keyField.getText()));
-                    break;
+                case "Hex" -> outputField.setText(decrypt.hex(inputField.getText()));
+                case "Binary" -> outputField.setText(decrypt.binary(inputField.getText()));
+                case "Base64" -> outputField.setText(decrypt.base64(inputField.getText()));
+                case "Rot13" -> outputField.setText(decrypt.rot13(inputField.getText()));
+                case "Caesar" ->
+                        outputField.setText(decrypt.caesar(inputField.getText(), Integer.parseInt(keyField.getText())));
+                case "Vigenere" -> outputField.setText(decrypt.vigenere(inputField.getText(), keyField.getText()));
+                case "Morse" -> outputField.setText(decrypt.morse(inputField.getText()));
+                case "Atbash" -> outputField.setText(decrypt.atbash(inputField.getText()));
+                case "Bacon" -> outputField.setText(decrypt.bacon(inputField.getText()));
+                case "XOR" -> outputField.setText(decrypt.xor(inputField.getText(), keyField.getText()));
+            }
+        });
+
+        saveButton.addActionListener(e -> {
+            FileHandler save = new FileHandler();
+            try {
+                save.save(inputField.getText(), keyField.getText(), outputField.getText(), comboBox.getSelectedItem().toString());
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        loadButton.addActionListener(e -> {
+            FileHandler load = new FileHandler();
+            try {
+                load.load(inputField, keyField, outputField, comboBox);
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         });
     }
